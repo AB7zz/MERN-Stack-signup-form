970e0d1781b5be17ef6358cd2cd3032574f64c32
AB7zz <48093418+AB7zz@users.noreply.github.com>
2021-07-25 13:56:03 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 const Connection = async (username, password) => {
-    const URL = `mongodb+srv://${username}:${password}@flipkart.itvvh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    const URL = `mongodb+srv://${username}:${password}@yourfoldername.itvvh.mongodb.net/yourclustername?retryWrites=true&w=majority`
     try{
         await mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
         console.log('Database connected successfully')
